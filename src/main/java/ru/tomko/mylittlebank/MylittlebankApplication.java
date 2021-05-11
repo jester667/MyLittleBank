8c7c8d1b2ed99e8c8e613cea566b5475344559f2
Tomko Vladislav <jester6671@gmail.com>
2021-05-11 23:25:50 +0400

    git init

---
+++ src/main/java/ru/tomko/mylittlebank/MylittlebankApplication.java
@@ -0,0 +1,13 @@
+package ru.tomko.mylittlebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylittlebankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MylittlebankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/tomko/mylittlebank/MylittlebankApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tomko.mylittlebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylittlebankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
