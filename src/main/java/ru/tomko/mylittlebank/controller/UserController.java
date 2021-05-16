ce0c51c0b361439db134537501466c4ed4d6b32a
Tomko Vladislav <jester6671@gmail.com>
2021-05-16 13:40:13 +0400

    add user controller and repository

---
+++ src/main/java/ru/tomko/mylittlebank/controller/UserController.java
@@ -0,0 +1,65 @@
+package ru.tomko.mylittlebank.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import ru.tomko.mylittlebank.dao.UserRepository;
+import ru.tomko.mylittlebank.entity.User;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<String> addUser(@RequestBody @Valid User user){
+        userRepository.save(user);
+        return ResponseEntity.ok("User data is valid");
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateEmployee(@RequestBody @Valid User user) {
+        userRepository.save(user);
+        return ResponseEntity.ok("User data is valid");
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Optional<User> getUser(@PathVariable int id) {
+        return userRepository.findById(id);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteUser(@PathVariable int id) {
+        userRepository.deleteById(id);
+    }
+
+    @GetMapping("/name/{name}")
+    public User getUserByFullName(@PathVariable String name) {
+        return userRepository.getByFullName(name);
+    }
+
+    @GetMapping("/phone/{phone}")
+    public User getUserByPhone(@PathVariable String phone) {
+        return userRepository.getByPhone(phone);
+    }
+
+    @GetMapping("/email/{email}")
+    public User getUserByEmail(@PathVariable String email) {
+        return userRepository.getByEmail(email);
+    }
+
+}
--- src/main/java/ru/tomko/mylittlebank/dao/UserRepository.java
+++ src/main/java/ru/tomko/mylittlebank/dao/UserRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.tomko.mylittlebank.entity.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    public User getByFullName(String name);
+    public User getByPhone(String phone);
+    public User getByEmail(String email);
 }
